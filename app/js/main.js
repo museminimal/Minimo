8210a5dabb3e0e18d98c4686e02f2fa04ada30ff
roman bogdanov <cflatminor92@gmail.com>
2019-08-01 13:34:50 +0300

    scrolltop header menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,31 @@
-let headerNavLinkButton = document.querySelectorAll('.nav-list .nav-item a'),
-    animationTime = 300,
-    framesCount = 20;
+/* SCROLLS FOR HEADER MENU */
 
-headerNavLinkButton.forEach(function (item) {
-    item.addEventListener('click', function (event) {
-        // console.log (event);
-        event.preventDefault();
-        // debugger;
-        let coordY = document.querySelector('.block2').getBoundingClientRect().top;
+$('a[href^="#lifestyle"]').on('click', function(event) {
+  var target = $( $(this).attr('href') );
+  if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: target.offset().top
+      }, 800);
+  }
+});
 
-        // запускаем интервал, в котором
-        let scroller = setInterval(function() {
-            // считаем на сколько скроллить за 1 такт
-            let scrollBy = coordY / framesCount;
+$('a[href^="#photodiary"]').on('click', function(event) {
+  var target = $( $(this).attr('href') );
+  if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: target.offset().top
+      }, 800);
+  }
+});
 
-            // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-            // и дно страницы не достигнуто
-            if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-                // то скроллим на к-во пикселей, которое соответствует одному такту
-                window.scrollBy(0, scrollBy);
-                // clearInterval(scroller);
-            } else {
-                // иначе добираемся до элемента и выходим из интервала
-                window.scrollTo(0, coordY);
-                clearInterval(scroller);
-            }
-            // время интервала равняется частному от времени анимации и к-ва кадров
-        }, animationTime / framesCount);
-    });
+$('a[href^="#travel"]').on('click', function(event) {
+  var target = $( $(this).attr('href') );
+  if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: target.offset().top
+      }, 600);
+  }
 });
